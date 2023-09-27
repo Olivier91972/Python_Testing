10a03c91cc8613c2408f21daba34302dc3cf6d93
Olivier Remy <olivier.remy.paris@gmail.com>
2023-09-27 15:43:47 +0200

    Book_past_competition Update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for, sessions
 
@@ -20,24 +21,31 @@ app.secret_key = 'something_special'
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+now = datetime.datetime.now()
+current_date = now.strftime("%Y-%m-%d, %H:%M:%S")
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
         club = [club for club in clubs if club['email'] == request.form['email']][0]
         return render_template('welcome.html',club=club,competitions=competitions)
 
+
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
+def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html', club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
+# Book_past_competition
 
 
 @app.route('/purchasePlaces',methods=['POST'])
